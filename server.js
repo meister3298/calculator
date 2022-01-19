e3747ae51edb864ed5359d4db6d79c6373447b6e
Chong Wooi <chongwooi@gmail.com>
2022-01-19 09:55:54 +0800

    Initial added.

---
+++ server.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/", function(req, res){
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = weight/(height*height);
+    res.send("Your BMI is " + bmi);
+})
+
+app.listen(3000, function(){
+    console.log("Started server on port 3000!");
+});
